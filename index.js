4ebd4679faf67f157597c411ab0dcc3e5df082f1
shubhjohri29 <97423939+shubhjohri29@users.noreply.github.com>
2022-08-03 17:31:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName:"shubh",
+    lastName: "johri",
+    fullName: function () {
+      // return this.firstName + " " + this.lastName;
+    }
+  }
+  console.log(person.fullname())
+  // person.fullName();
+  
